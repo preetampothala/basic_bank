885f4524d3efe7f87cbf7f032b37c7b12dce0e63
Preetam Pothala <preetamreddy.pothala@gmail.com>
2022-10-18 03:20:28 -0600

    local storage

--- account.js
+++ account.js
@@ -1,99 +1,103 @@
 let count = 1;
 class account {
-	loan(amount) {
-		let text = ``;
-		if (
-			amount > 0 &&
-			this.transactions.some((transaction) => transaction >= amount * 0.1)
-		) {
-			this.transactions.push(amount);
-		} else {
-			if (!amount > 0) {
-				text = `Enter amount greater than Zero`;
-			} else if (
-				!this.transactions.some((transaction) => transaction >= amount * 0.1)
-			) {
-				text = `Your loan request has been rejected because it doesn't meet the minimum 10% of your current balance`;
-			}
-			return text;
-		}
-	}
-	transfer(amount, receiverAccount) {
-		let text = ``;
-		if (
-			amount > 0 &&
-			receiverAccount &&
-			this.balance >= amount &&
-			receiverAccount?.username !== this.username
-		) {
-			console.log("Transfer valid");
-			receiverAccount.transactions.push(Number(amount));
-			console.log(receiverAccount.transactions);
-			this.transactions.push(-Number(amount));
-			return (text = `success`);
-		} else {
-			if (!amount > 0) text = `Please enter an amount greater than 0`;
-			else if (!receiverAccount) text = `Please enter a valid To account name`;
-			else if (!(this.balance >= amount))
-				text = `Please enter an amount less than your current balance`;
-			else if (!(receiverAccount?.username !== this.username))
-				text = `Please enter a different To account name that your current account`;
-			return text;
-		}
-	}
-	sortTrans(sort) {
-		console.log(sort);
-		return sort
-			? this.transactions.slice().sort((a, b) => a - b)
-			: this.transactions;
-	}
-	constructor(owner, interestrate, transactions) {
-		this.owner = owner;
-		this.transactions = transactions;
-		this.interestrate = interestrate;
-		this.generateusername();
-		this.generatepin();
-		this.calcBalance();
-		this.calcSummary();
-	}
-	generateusername() {
-		const username = this.owner
-			.toLowerCase()
-			.split(" ")
-			.map((name) => name[0] + name[1])
-			.join("");
-		this.username = username;
-	}
-	generatepin() {
-		let pin = count * 1111;
-		this.pin = pin;
-		count++;
-	}
-	calcBalance() {
-		this.balance = this.transactions.reduce((acc, cur) => acc + cur, 0);
-		return this.balance;
-	}
-	calcSummary() {
-		this.in = Math.abs(
-			this.transactions
-				.filter((transaction) => transaction > 0)
-				.reduce((acc, cur) => acc + cur, 0)
-		).toFixed(2);
-		this.out = Math.abs(
-			this.transactions
-				.filter((transaction) => transaction < 0)
-				.reduce((acc, cur) => acc + cur, 0)
-				.toFixed(2)
-		);
-		this.interest = Math.abs(
-			this.transactions
-				.filter((transaction) => transaction > 0)
-				.map((transaction) => (transaction * this.interestrate) / 100)
-				.reduce((acc, cur) => acc + cur, 0)
-				.toFixed(2)
-		);
-		return { in: this.in, out: this.out, interest: this.interest };
-	}
+  constructor(owner, interestrate, transactions) {
+    this.owner = owner;
+    this.transactions = transactions;
+    this.interestrate = interestrate;
+    this.generateusername();
+    this.generatepin();
+    this.calcBalance();
+    this.calcSummary();
+  }
+  generateusername() {
+    const username = this.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0] + name[1])
+      .join("");
+    this.username = username;
+  }
+  generatepin() {
+    let pin = count * 1111;
+    this.pin = pin;
+    count++;
+  }
+  calcBalance() {
+    this.balance = this.transactions.reduce((acc, cur) => acc + cur, 0);
+    return this.balance;
+  }
+  calcSummary() {
+    this.in = Math.abs(
+      this.transactions
+        .filter((transaction) => transaction > 0)
+        .reduce((acc, cur) => acc + cur, 0)
+    ).toFixed(2);
+    this.out = Math.abs(
+      this.transactions
+        .filter((transaction) => transaction < 0)
+        .reduce((acc, cur) => acc + cur, 0)
+        .toFixed(2)
+    );
+    this.interest = Math.abs(
+      this.transactions
+        .filter((transaction) => transaction > 0)
+        .map((transaction) => (transaction * this.interestrate) / 100)
+        .reduce((acc, cur) => acc + cur, 0)
+        .toFixed(2)
+    );
+    return { in: this.in, out: this.out, interest: this.interest };
+  }
+  loan(amount) {
+    console.log(`loan`);
+    let result = ``;
+    if (
+      amount > 0 &&
+      this.transactions.some((transaction) => transaction >= amount * 0.1)
+    ) {
+      console.log(`loan valid`);
+      this.transactions.push(amount);
+      result = `success`;
+      return result;
+    } else {
+      console.log(`loan invalid`);
+      if (!(amount > 0 === true)) result = `Enter amount greater than Zero`;
+      else if (
+        !this.transactions.some((transaction) => transaction >= amount * 0.1)
+      )
+        result = `Your loan request has been rejected because it doesn't meet the criteria`;
+
+      return result;
+    }
+  }
+  transfer(amount, receiverAccount) {
+    let text = ``;
+    if (
+      amount > 0 &&
+      receiverAccount &&
+      this.balance >= amount &&
+      receiverAccount?.username !== this.username
+    ) {
+      console.log("Transfer valid");
+      receiverAccount.transactions.push(Number(amount));
+      console.log(receiverAccount.transactions);
+      this.transactions.push(-Number(amount));
+      return (text = `success`);
+    } else {
+      if (!amount > 0) text = `Please enter an amount greater than 0`;
+      else if (!receiverAccount) text = `Please enter a valid To account name`;
+      else if (!(this.balance >= amount))
+        text = `Please enter an amount less than your current balance`;
+      else if (!(receiverAccount?.username !== this.username))
+        text = `Please enter a different To account name that your current account`;
+      return text;
+    }
+  }
+  sortTrans(sort) {
+    console.log(sort);
+    return sort
+      ? this.transactions.slice().sort((a, b) => a - b)
+      : this.transactions;
+  }
 }
 
 export default account;
--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const sortBtnElem = document.querySelector(".btn-sort");
 const summaryValueInElem = document.querySelector(".summaryvaluein");
 const summaryValueOutElem = document.querySelector(".summaryvalueout");
 const summaryValueInterestElem = document.querySelector(
-	".summaryvalueinterest"
+  ".summaryvalueinterest"
 );
 const transactionsElem = document.querySelector(".transactions");
 const appCloseElem = document.querySelector(".appclose");
@@ -28,217 +28,228 @@ const appErrorMsgElem = document.querySelector(".apperrormessage");
 const loginCloseelem = document.querySelector(".loginclose");
 const loginErrorMsgElem = document.querySelector(".loginerrormessage");
 let sort = true;
-console.log(sort);
 
 class App {
-	#accounts = [];
-	#currentAccount;
-	constructor() {
-		// this._getLocalStorage();
-		this._generateaccounts();
-		loginBtnElem.addEventListener("click", this._login.bind(this));
-		transferBtnElem.addEventListener("click", this._transfer.bind(this));
-		loanBtnElem.addEventListener("click", this._loan.bind(this));
-		appCloseElem.addEventListener("click", this._closeErrormsg);
-		loginCloseelem.addEventListener("click", this._closeErrormsg);
-		sortBtnElem.addEventListener("click", this._sortTransactions.bind(this));
-	}
-	_login(e) {
-		e.preventDefault();
-		let text = ``;
-		const username = loginUserElem.value;
-		const pin = Number(loginPinElem.value);
-		console.log(username, pin);
-		this.#currentAccount = this.#accounts.find(
-			(acc) => acc.username === username && acc.pin === pin
-		);
-		console.log(this.#currentAccount);
-		if (this.#currentAccount) {
-			if (this.#currentAccount?.pin === Number(pin)) {
-				welcomeTextElem.textContent = `Welcome back, ${
-					this.#currentAccount.owner.split(" ")[0]
-				}`;
-			}
-		} else {
-			if (!this.#currentAccount) text = `No such account exists`;
-			else if (!(this.#currentAccount?.pin === Number(pin)))
-				text = `Please enter a correct pin`;
-			this._displayErrorMsg(loginErrorMsgElem, text);
-			// alert(`Login Error. ${text}`);
-			// errorMsgElem.innerHTML = "";
-			// errorMsgElem.innerHTML = `<p>Login Error. s<span style="font-weight:600">${text}</span></p>`;
-		}
-		loginUserElem.value = loginPinElem.value = "";
-		loginPinElem.blur();
-		this._updateUI(this.#currentAccount);
+  #accounts = [];
+  #currentAccount;
+  constructor() {
+    this._getLocalStorage();
+    loginBtnElem.addEventListener("click", this._login.bind(this));
+    transferBtnElem.addEventListener("click", this._transfer.bind(this));
+    loanBtnElem.addEventListener("click", this._loan.bind(this));
+    appCloseElem.addEventListener("click", this._closeErrormsg);
+    loginCloseelem.addEventListener("click", this._closeErrormsg);
+    sortBtnElem.addEventListener("click", this._sortTransactions.bind(this));
+  }
+  _login(e) {
+    e.preventDefault();
+    let text = ``;
+    const username = loginUserElem.value;
+    const pin = Number(loginPinElem.value);
+    console.log(username, pin);
+    this.#currentAccount = this.#accounts.find(
+      (acc) => acc.username === username && acc.pin === pin
+    );
+    // console.log(this.#currentAccount);
+    if (this.#currentAccount) {
+      if (this.#currentAccount?.pin === Number(pin)) {
+        welcomeTextElem.textContent = `Welcome back, ${
+          this.#currentAccount.owner.split(" ")[0]
+        }`;
+      }
+    } else {
+      if (!this.#currentAccount) text = `No such account exists`;
+      else if (!(this.#currentAccount?.pin === Number(pin)))
+        text = `Please enter a correct pin`;
+      this._displayErrorMsg(loginErrorMsgElem, text);
+      // alert(`Login Error. ${text}`);
+      // errorMsgElem.innerHTML = "";
+      // errorMsgElem.innerHTML = `<p>Login Error. s<span style="font-weight:600">${text}</span></p>`;
+    }
+    loginUserElem.value = loginPinElem.value = "";
+    loginPinElem.blur();
+    this._updateUI(this.#currentAccount);
 
-		appElem.style.display = "block";
-		// appElem.style.opacity = 100;
-	}
+    appElem.style.display = "block";
+    // appElem.style.opacity = 100;
+  }
 
-	_transfer(e) {
-		e.preventDefault();
-		const amount = Number(transferAmtElem.value);
-		const receiver = transferToElem.value;
-		console.log(amount, receiver);
-		const receiverAccount = this.#accounts.find(
-			(acc) => acc.username === receiver
-		);
-		console.log(receiverAccount, this.#currentAccount);
-		console.log(
-			amount > 0,
-			this.#currentAccount.balance,
-			this.#currentAccount.balance >= amount,
-			receiverAccount?.username !== this.#currentAccount.username
-		);
-		let transferReturn = this.#currentAccount.transfer(amount, receiverAccount);
-		if (transferReturn === `success`) {
-			console.log(receiverAccount.transactions);
-			// receiverAccount.transactions.push(Number(amount));
-			this._updateUI(this.#currentAccount);
-		} else {
-			this._displayErrorMsg(appErrorMsgElem, transferReturn);
-		}
-		// if (
-		// 	amount > 0 &&
-		// 	receiverAccount &&
-		// 	this.#currentAccount.balance >= amount &&
-		// 	receiverAccount?.username !== this.#currentAccount.username
-		// ) {
-		// 	console.log("Transfer valid");
-		// 	receiverAccount.transactions.push(Number(amount));
-		// 	this.#currentAccount.transactions.push(-Number(amount));
-		// 	this._updateUI(this.#currentAccount);
-		// 	appErrorMsgElem.querySelector("p").textContent = "";
-		// } else {
-		// 	let text = ``;
-		// 	if (!amount > 0) text = `Please enter an amount greater than 0`;
-		// 	else if (!receiverAccount) text = `Please enter a valid To account name`;
-		// 	else if (!(this.#currentAccount.balance >= amount))
-		// 		text = `Please enter an amount less than your current balance`;
-		// 	else if (!(receiverAccount?.username !== this.#currentAccount.username))
-		// 		text = `Please enter a different To account name that your current account`;
-		transferAmtElem.value = transferToElem.value = "";
-		this._setLocalStorage();
-	}
+  _transfer(e) {
+    e.preventDefault();
+    const amount = Number(transferAmtElem.value);
+    const receiver = transferToElem.value;
+    console.log(amount, receiver);
+    const receiverAccount = this.#accounts.find(
+      (acc) => acc.username === receiver
+    );
+    console.log(receiverAccount, this.#currentAccount);
+    console.log(
+      amount > 0,
+      this.#currentAccount.balance,
+      this.#currentAccount.balance >= amount,
+      receiverAccount?.username !== this.#currentAccount.username
+    );
+    let transferReturn = this.#currentAccount.transfer(amount, receiverAccount);
+    if (transferReturn === `success`) {
+      console.log(receiverAccount.transactions);
+      // receiverAccount.transactions.push(Number(amount));
+      this._updateUI(this.#currentAccount);
+    } else {
+      this._displayErrorMsg(appErrorMsgElem, transferReturn, "Transfer");
+    }
+    // if (
+    // 	amount > 0 &&
+    // 	receiverAccount &&
+    // 	this.#currentAccount.balance >= amount &&
+    // 	receiverAccount?.username !== this.#currentAccount.username
+    // ) {
+    // 	console.log("Transfer valid");
+    // 	receiverAccount.transactions.push(Number(amount));
+    // 	this.#currentAccount.transactions.push(-Number(amount));
+    // 	this._updateUI(this.#currentAccount);
+    // 	appErrorMsgElem.querySelector("p").textContent = "";
+    // } else {
+    // 	let text = ``;
+    // 	if (!amount > 0) text = `Please enter an amount greater than 0`;
+    // 	else if (!receiverAccount) text = `Please enter a valid To account name`;
+    // 	else if (!(this.#currentAccount.balance >= amount))
+    // 		text = `Please enter an amount less than your current balance`;
+    // 	else if (!(receiverAccount?.username !== this.#currentAccount.username))
+    // 		text = `Please enter a different To account name that your current account`;
+    transferAmtElem.value = transferToElem.value = "";
+    this._setLocalStorage();
+  }
 
-	_loan(e) {
-		e.preventDefault();
-		const amount = Number(loanAmtElem.value);
-		console.log(
-			this.#currentAccount.transactions.some(
-				(transaction) => transaction >= amount * 0.1
-			)
-		);
-		let loanreturn = this.#currentAccount.loan(amount);
-		this._updateUI(this.#currentAccount);
+  _loan(e) {
+    console.log("Loan");
+    e.preventDefault();
+    const amount = Number(loanAmtElem.value);
+    // console.log(
+    //   this.#currentAccount.transactions.some(
+    //     (transaction) => transaction >= amount * 0.1
+    //   )
+    // );
 
-		// if (
-		// 	amount > 0 &&
-		// 	this.#currentAccount.transactions.some(
-		// 		(transaction) => transaction >= amount * 0.1
-		// 	)
-		// ) {
-		// 	this.#currentAccount.transactions.push(amount);
-		// 	this._updateUI(this.#currentAccount);
-		// } else {
-		// }
-		this._setLocalStorage();
-		loanAmtElem.value = "";
-	}
-	_updateUI(acc) {
-		// this._closeerrormsg();
-		this._displayBalance(acc);
-		this._displaysummary(acc);
-		this._displayTransactions(acc.transactions);
-	}
-	_generateaccounts() {
-		// accountcreationArray.forEach((acc) => {
-		// 	this.#accounts.push(
-		// 		new account(acc.owner, acc.interestRate, acc.transactions)
-		// 	);
-		// });
-    
-	}
+    let loanreturn = this.#currentAccount.loan(amount);
+    if (loanreturn === `success`) {
+      this._updateUI(this.#currentAccount);
+    } else {
+      this._displayErrorMsg(appErrorMsgElem, loanreturn, "Loan");
+    }
 
-	_displayBalance(acc) {
-		// acc.balance = acc.transactions.reduce((acc, cur) => acc + cur, 0);
-		// console.log(this.#currentAccount.balance);
-		let balance = acc.calcBalance();
-		console.log(balance);
-		balanceElem.textContent = `${Math.abs(balance).toFixed(2)}$`;
-		if (balance < 0) balanceElem.style.color = "red";
-		else balanceElem.style.color = "green";
-		console.log(acc);
-	}
-	_displaysummary(acc) {
-		// acc.in = Math.abs(
-		// 	acc.transactions
-		// 		.filter((transaction) => transaction > 0)
-		// 		.reduce((acc, cur) => acc + cur, 0)
-		// ).toFixed(2);
-		// acc.out = Math.abs(
-		// 	acc.transactions
-		// 		.filter((transaction) => transaction < 0)
-		// 		.reduce((acc, cur) => acc + cur, 0)
-		// 		.toFixed(2)
-		// );
-		// acc.interest = Math.abs(
-		// 	acc.transactions
-		// 		.filter((transaction) => transaction > 0)
-		// 		.map((transaction) => (transaction * acc.interestrate) / 100)
-		// 		.reduce((acc, cur) => acc + cur, 0)
-		// 		.toFixed(2)
-		// );
-		let { in: inward, out: outward, interest: interest } = acc.calcSummary();
-		summaryValueInElem.textContent = `${inward}$`;
-		summaryValueOutElem.textContent = `${Math.abs(outward)}$`;
-		summaryValueInterestElem.textContent = `${interest}$`;
-	}
-	_sortTransactions() {
-		// console.log(this.#currentAccount);
-		console.log(sort);
-		let sortResult = this.#currentAccount.sortTrans(sort);
-		console.log(sortResult);
-		this._displayTransactions(sortResult);
-		sort = !sort;
-		console.log(sort);
-	}
-	_displayTransactions(transactions) {
-		transactionsElem.innerHTML = "";
-		transactions.forEach(function (transaction, i) {
-			const type = transaction > 0 ? "Deposit" : "Withdrawl";
-			const html = `<div class="transactions-row ${type}">
+    // if (
+    // 	amount > 0 &&
+    // 	this.#currentAccount.transactions.some(
+    // 		(transaction) => transaction >= amount * 0.1
+    // 	)
+    // ) {
+    // 	this.#currentAccount.transactions.push(amount);
+    // 	this._updateUI(this.#currentAccount);
+    // } else {
+    // }
+    this._setLocalStorage();
+    loanAmtElem.value = "";
+  }
+  _updateUI(acc) {
+    // this._closeerrormsg();
+    this._displayBalance(acc);
+    this._displaysummary(acc);
+    this._displayTransactions(acc.transactions);
+  }
+
+  _displayBalance(acc) {
+    // acc.balance = acc.transactions.reduce((acc, cur) => acc + cur, 0);
+    // console.log(this.#currentAccount.balance);
+    let balance = acc.calcBalance();
+    // console.log(balance);
+    balanceElem.textContent = `${Math.abs(balance).toFixed(2)}$`;
+    if (balance < 0) balanceElem.style.color = "red";
+    else balanceElem.style.color = "green";
+    // console.log(acc);
+  }
+  _displaysummary(acc) {
+    // acc.in = Math.abs(
+    // 	acc.transactions
+    // 		.filter((transaction) => transaction > 0)
+    // 		.reduce((acc, cur) => acc + cur, 0)
+    // ).toFixed(2);
+    // acc.out = Math.abs(
+    // 	acc.transactions
+    // 		.filter((transaction) => transaction < 0)
+    // 		.reduce((acc, cur) => acc + cur, 0)
+    // 		.toFixed(2)
+    // );
+    // acc.interest = Math.abs(
+    // 	acc.transactions
+    // 		.filter((transaction) => transaction > 0)
+    // 		.map((transaction) => (transaction * acc.interestrate) / 100)
+    // 		.reduce((acc, cur) => acc + cur, 0)
+    // 		.toFixed(2)
+    // );
+    let { in: inward, out: outward, interest: interest } = acc.calcSummary();
+    summaryValueInElem.textContent = `${inward}$`;
+    summaryValueOutElem.textContent = `${Math.abs(outward)}$`;
+    summaryValueInterestElem.textContent = `${interest}$`;
+  }
+  _sortTransactions() {
+    // console.log(this.#currentAccount);
+    console.log(sort);
+    let sortResult = this.#currentAccount.sortTrans(sort);
+    console.log(sortResult);
+    this._displayTransactions(sortResult);
+    sort = !sort;
+    console.log(sort);
+  }
+  _displayTransactions(transactions) {
+    transactionsElem.innerHTML = "";
+    transactions.forEach(function (transaction, i) {
+      const type = transaction > 0 ? "Deposit" : "Withdrawl";
+      const html = `<div class="transactions-row ${type}">
       <div>
         ${i + 1}
       </div>
       <div class="transactions-type transactions-type-${type}">${type}</div>
       <div class="transactions-value">${transaction}$</div>
     </div>`;
-			transactionsElem.insertAdjacentHTML("afterbegin", html);
-		});
-	}
-	_displayErrorMsg(elem, msg) {
-		elem.querySelector("p").textContent = "";
-		console.log("Transfer invalid");
-		elem.querySelector("p").textContent = `Transfer Error. ${msg}`;
-		elem.style.display = "flex";
-	}
-	_closeErrormsg(e) {
-		appErrorMsgElem.style.display = "none";
-		loginErrorMsgElem.style.display = "none";
-		// errorMsgElem.style.opacity = 0;
-		// this._updateUI(this.#currentAccount);
-	}
-	_setLocalStorage() {
-		localStorage.setItem("accounts", JSON.stringify(this.#accounts));
-	}
-	_getLocalStorage() {
-		const data = JSON.parse(localStorage.getItem("accounts"));
-		if (!data) return;
-		console.log(data);
-		this.#accounts = data;
-	}
+      transactionsElem.insertAdjacentHTML("afterbegin", html);
+    });
+  }
+  _displayErrorMsg(elem, msg, type) {
+    elem.querySelector("p").textContent = "";
+    elem.querySelector("p").textContent = `${type} Error. ${msg}`;
+    elem.style.display = "flex";
+  }
+  _closeErrormsg(e) {
+    appErrorMsgElem.style.display = "none";
+    loginErrorMsgElem.style.display = "none";
+  }
+  _setLocalStorage() {
+    const accounts = [];
+    this.#accounts.forEach((account) => {
+      accounts.push({
+        owner: account.owner,
+        transactions: account.transactions,
+        interestRate: account.interestrate,
+      });
+    });
+    localStorage.setItem("accounts", JSON.stringify(accounts));
+  }
+  _getLocalStorage() {
+    const current_accounts = JSON.parse(localStorage.getItem("accounts"));
+    if (current_accounts && current_accounts.length > 0) {
+      current_accounts.forEach((acc) => {
+        this.#accounts.push(
+          new account(acc.owner, acc.interestRate, acc.transactions)
+        );
+      });
+    } else {
+      INITIAL_ACCOUNTS.forEach((acc) => {
+        this.#accounts.push(
+          new account(acc.owner, acc.interestRate, acc.transactions)
+        );
+      });
+    }
+  }
 }
 
 let app = new App();
