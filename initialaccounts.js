7e5ab846a9f7f921278cce776b8bcff141da234f
Preetam Pothala <preetamreddy.pothala@gmail.com>
2022-10-27 09:14:41 -0600

    Update initialaccounts.js

--- initialaccounts.js
+++ initialaccounts.js
@@ -1,21 +1,21 @@
 const INITIAL_ACCOUNTS = [
   {
-    name: "Naveen Nekkalapu",
+    name: "John Doe",
     transactions: [200, 455.23, -306.5, -642.21, -133.9, 79.97, 1300],
     interestRate: 1.2,
   },
   {
-    name: "Vijaya Veerisetty",
+    name: "Rickey Thomas Cooper",
     transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
   },
   {
-    name: "Anita Taladevulapalli",
+    name: "Edris Beckford",
     transactions: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 1.4,
   },
   {
-    name: "Akshay Krishna",
+    name: "Carol Muriel Mattes",
     transactions: [430, 1000, 700, 50, 90],
     interestRate: 1.6,
   },
